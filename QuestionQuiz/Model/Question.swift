a392323207b2098a6034d5fb46e3ecc777f3dd58
Alex Sobolev <31108@mail.ru>
2023-11-06 17:39:22 +0300

    Setup Model

--- QuestionQuiz/Model/Question.swift
+++ QuestionQuiz/Model/Question.swift
@@ -5,4 +5,71 @@
 //  Created by Александр Соболев on 06.11.2023.
 //
 
-import Foundation
+struct Question {
+    let title: String
+    let answers: [Answer]
+    let type: ResponseType
+    
+    static func getQuestions() -> [Question] {
+        [
+        Question(
+            title: "Какую пищу вы предпочитаете?",
+            answers: [
+                Answer(title: "Стейк", animal: .dog),
+                Answer(title: "Рыба", animal: .cat),
+                Answer(title: "Морковь", animal: .rabbit),
+                Answer(title: "Кукуруза", animal: .turtle),
+            ], type: .single
+        ),
+        Question(
+            title: "Что вам нравится больше?",
+            answers: [
+                Answer(title: "Плавать", animal: .dog),
+                Answer(title: "Спать", animal: .cat),
+                Answer(title: "Обнимать", animal: .rabbit),
+                Answer(title: "Есть", animal: .turtle)
+            ], type: .multiple
+        ),
+        Question(
+            title: "Любите ли вы поездки на автомобиле?",
+            answers: [
+                Answer(title: "Ненавижу", animal: .cat),
+                Answer(title: "Нервничаю", animal: .rabbit),
+                Answer(title: "Нормально", animal: .turtle),
+                Answer(title: "Обожаю", animal: .dog)
+            ], type: .ranged
+        )
+        ]
+    }
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравится быть с друзьями"
+        case .cat:
+            return "Вы себе на уме"
+        case .rabbit:
+            return "Вы активны и за здоровый образ жизни"
+        case .turtle:
+            return "Кто понял жизнь - тот не спешит"
+        }
+    }
+}
